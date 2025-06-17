a32c2c1b9d8b953cc807079f441fb4c252cde44e
shashankshetty1 <shettyshashank089@gmail.com>
2025-06-17 10:09:36 +0530

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/BuilderPortfolio/" : "/",
-  server: {
+   base: "/",
+   server: {
     host: "::",
     port: 8080,
   },
